23db4e541906fa05739f91c122da2f405b7e14c9
Tianzheng Wang <tzwang@sfu.ca>
2021-10-07 14:11:41 -0700

    Setup milestone 2.

---
+++ Index/pskiplist.h
@@ -0,0 +1,120 @@
+/*
+ * YASE: Yet Another Storage Engine
+ *
+ * CMPT 454 Database Systems II Course Project, Fall 2021
+ *
+ * Copyright (C) School of Computing Science, Simon Fraser University
+ *
+ * Not for distribution without prior approval.
+ */
+#pragma once
+
+#include <gtest/gtest_prod.h>
+
+#include "../yase_internal.h"
+
+namespace yase {
+
+// Skip list node (tower)
+struct PSkipListNode {
+  static const uint8_t kInvalidLevels = 0;
+
+  // Tower height
+  uint32_t nlevels;
+
+  // Payload (RID)
+  RID rid;
+
+  // Pointer to the next node. The i-th element represents the (i+1)th level
+  RID next[SKIP_LIST_MAX_LEVEL];
+
+  // Key (must be the last field of this struct)
+  char key[0];
+
+  PSkipListNode(uint32_t nlevels, RID rid) : nlevels(nlevels), rid(rid) {}
+  PSkipListNode() : nlevels(kInvalidLevels) {}
+  ~PSkipListNode() {}
+};
+
+// Skip list that maps keys to RIDs
+class PSkipList {
+ public:
+  // Constructor - create a skip list
+  PSkipList(std::string name, uint32_t key_size);
+
+  // Destructor
+  ~PSkipList();
+
+  // Insert a key - RID mapping into the skip list index
+  // @key: pointer to the key
+  // @rid: RID of the record represented by the key (the "data entry")
+  bool Insert(const char *key, RID rid);
+
+  // Search for a key in the skip list
+  // @key: pointer to the key
+  // Returns the data entry corresponding to the search key; returns Invalid RID
+  // if the key is not found
+  RID Search(const char *key);
+
+  // Delete a key from the index
+  // @key: pointer to the key
+  // Returns true if the index entry is successfully deleted, false if the key
+  // does not exist
+  bool Delete(const char *key);
+
+  // Update the data entry with a new rid
+  // @key: target key
+  // @rid: new RID to be written
+  // Returns true if the update was successful
+  bool Update(const char *key, RID rid);
+
+  // Scan and return multiple keys/payloads
+  // @start_key: start (smallest) key of the scan operation
+  // @nkeys: number of keys to scan
+  // @inclusive: whether the result (nkeys) includes [start_key] (and the RID)
+  // @out_records: pointer to a vector to stores the scan result (RIDs)
+  void ForwardScan(const char *start_key, uint32_t nkeys, bool inclusive,
+                   std::vector<std::pair<char *, RID> > *out_records);
+
+  // Traverse the skip list to find a key
+  // @key: pointer to the target key
+  // @out_pred_nodes: a vector (provided by the caller) to store the predecessor
+  // nodes during traversal), sorted by reverse-height order, i.e.,
+  // out_pred_nodes[0] should point to the height-level predecessor (the head
+  // dummy node), and out_pred_nodes[1] should point to the second height tower
+  // and so on.
+  // Returns the node containing the provided key if the key exists; otherwise
+  // returns nullptr.
+  RID Traverse(const char *key, std::vector<RID> *out_pred_nodes = nullptr);
+
+  // Create a new skip list node (tower)
+  // @levels: height of this tower
+  // @key: pointer to the key
+  // @rid: data entry (RID)
+  RID NewNode(uint32_t levels, const char *key, RID rid);
+
+ private:
+  FRIEND_TEST(PSkipListTest, Init);
+  FRIEND_TEST(PSkipListTest, NewNode);
+  FRIEND_TEST(PSkipListTest, SortedList);
+  FRIEND_TEST(PSkipListTest, ForwardScanInclusive);
+  FRIEND_TEST(PSkipListTest, ForwardScanNonInclusive);
+  FRIEND_TEST(PSkipListTest, Hidden1);
+  FRIEND_TEST(PSkipListTest, Hidden2);
+  FRIEND_TEST(PSkipListTest, Hidden3);
+  FRIEND_TEST(PSkipListTest, Hidden4);
+
+  // key size supported
+  uint32_t key_size;
+
+  // Dummy head tower
+  RID head;
+
+  // Dummy tail tower
+  RID tail;
+
+  // Current height of the skip list
+  uint32_t height;
+};
+
+}  // namespace yase
---
+++ Index/skiplist.cc
@@ -0,0 +1,125 @@
+/*
+ * YASE: Yet Another Storage Engine
+ *
+ * CMPT 454 Database Systems II, Fall 2021
+ *
+ * Copyright (C) School of Computing Science, Simon Fraser University
+ *
+ * Not for distribution without prior approval.
+ */
+
+#include <random>
+#include "skiplist.h"
+
+namespace yase {
+
+SkipList::SkipList(uint32_t key_size) {
+  // Initialize skip list with necessary parameters:
+  // 1. ey_size as the argument passed in
+  // 2. Initial height to be 1
+  // 3. Initialize each level to be an empty linked list 
+  //    (i.e., heads pointing to tails, tails pointing to null)
+  // 4. Initialize latches
+  //
+  // TODO: Your implementation
+}
+
+SkipList::~SkipList() {
+  // Deallocate all the towers allocated in memory and destroy latches
+  //
+  // TODO: Your implementation
+}
+
+SkipListNode *SkipList::NewNode(uint32_t levels, const char *key, RID rid) {
+  // 1. Use malloc to allocate the a node, including SkipListNode itself and the
+  //    key that follows (i.e., starts at SkipListNode.key).
+  // 2. Use placement new and the SkipListNode(uint32_t nlevels, RID rid)
+  //    constructor to initialize the node
+  // 3. Copy the key to the node's key area (represented by the "key" variable)
+  //
+  // TODO: Your implementation
+  return nullptr;
+}
+
+SkipListNode *SkipList::Traverse(const char *key, std::vector<SkipListNode*> *out_pred_nodes) {
+  // Start from the head dummy tower to reach the specified key. If the key
+  // exists, return the tower that represents the key, otherwise return nullptr
+  // to indicate the key does not exist.
+  //
+  // Keep predecessor nodes (i.e., whenever the search path "makes a turn") in the
+  // user-provided vector. Note that out_pred_nodes might be nullptr if the user
+  // is Read() or Update for which the predecessor nodes won't be useful.
+  //
+  // TODO: Your implementation
+  return nullptr;
+}
+
+bool SkipList::Insert(const char *key, RID rid) {
+  // Use the Traverse function to reach the insert position:
+  // 1. If Traverse returns a valid node, then the key already exists in the skip
+  //    list and return false;
+  // 2. Otherwise continue to:
+  //    (a) determine the height of the new tower
+  //    (b) build up the tower from bottom up using the vector passed in to the
+  //        Traverse function: it should contain all the predecessors at each
+  //        level
+  //    (c) return true/false to indicate a successful/failed insert
+  //
+  // TODO: Your implementation
+  return false;
+}
+
+RID SkipList::Search(const char *key) {
+  // Use the Traverse function to locate the key.
+  // Return the RID (i.e., payload) if the key is found; otherwise return invalid RID.
+  //
+  // TODO: Your implementation
+  return RID();
+}
+
+bool SkipList::Update(const char *key, RID rid) {
+  // Invoke the Traverse function to obtain the target tower and update its
+  // payload RID. 
+  //
+  // Return true if the key is found and the RID is updated,
+  // otherwise return false.
+  //
+  // TODO: Your implementation
+  return false;
+}
+
+bool SkipList::Delete(const char *key) {
+  // Similar to Insert(), use the Traverse function to obtain the target node
+  // that contains the provided key and the predecessor nodes, then unlink the
+  // returned node at all levels.
+  //
+  // The unlinked tower should be freed.
+  //
+  // Return true if the operation succeeeded, false if the key is not found.
+  //
+  // TODO: Your implementation
+  return false;
+}
+
+void SkipList::ForwardScan(const char *start_key, uint32_t nkeys, bool inclusive,
+                           std::vector<std::pair<char *, RID> > *out_records) {
+  // 1. Use the Traverse function to locate the start key and a stack of
+  //    predecessor nodes.
+  // 2. Scan from the start key and push results in the given result set
+  //    (out_records vector).
+  //
+  // Notes: 
+  // 1. Return directly if [out_records] is null or nkeys is 0.
+  // 2. If inclusive is set to fales, the start_key record should be excluded
+  //    from the result set.
+  // 3. Results (elements in [out_records]) should be sorted in ascending key
+  //    order
+  // 4. The keys stored in the result vector should be copies of keys in skip
+  //    list nodes using dynamically allocated memory. Caller of ForwardScan 
+  //    is responsible for deallocating the keys after use. 
+  // 5. A start key of null means scanning from the smallest record.
+  //
+  // TODO: Your implementation
+}
+
+}  // namespace yase
