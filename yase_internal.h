3821527bcc8baa7f10c88597caf37e425e864747
Tianzheng Wang <tzwang@sfu.ca>
2021-11-02 23:45:04 -0700

    Setup milestone 3.

--- yase_internal.h
+++ yase_internal.h
@@ -9,6 +9,8 @@
  */
 #pragma once
 
+#include <atomic>
+#include <list>
 #include <glog/logging.h>
 
 namespace yase {
@@ -60,5 +62,41 @@ struct RID {
   static inline RID MakeRid(PageId page_id, uint32_t slot_num) {
     return RID(page_id.value | slot_num);
   }
+};
+
+struct Transaction {
+  static std::atomic<uint64_t> ts_counter;
+  const static uint32_t kStateCommitted = 1;
+  const static uint32_t kStateInProgress = 2;
+  const static uint32_t kStateAborted = 3;
+  const static uint64_t kInvalidTimestamp = ~uint64_t{0};
+
+  // Transaction timestamp. Smaller == older.
+  std::atomic<uint64_t> timestamp;
+
+  // List of the RID of records which the transaction currently locked.
+  // Other implementations may maintain a list of LockRequest references;
+  // for this project we use RIDs.
+  std::list<RID> locks;
+
+  // State of the transaction, must be one of the kState* values
+  uint32_t state;
+
+  Transaction() : timestamp(ts_counter.fetch_add(1)), state(kStateInProgress) {}
+  ~Transaction() {}
+
+  // Abort the transaction
+  // Returns the timestamp of this transaction.
+  // If the operation fails, return invalid timestamp (kInvalidTimestamp)
+  uint64_t Abort();
+
+  // Commit the transaction
+  // Returns true/false if the operation succeeded/failed.
+  bool Commit();
+
+  inline uint64_t GetTimestamp() { return timestamp; }
+  inline bool IsAborted() { return state == kStateAborted; }
+  inline bool IsCommitted() { return state == kStateCommitted; }
+  inline bool IsInProgress() { return state == kStateInProgress; }
 };
 }  // namespace yase
